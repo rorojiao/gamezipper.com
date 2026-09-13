fa32844424d73e5f5f6450fadd4faafffa6f18a4
Junze <58159997+rorojiao@users.noreply.github.com>
2026-09-13 20:14:06 +0900

    R574 catalog: js/itemlist-schema.js

--- js/itemlist-schema.js
+++ js/itemlist-schema.js
@@ -463,8 +463,9 @@ const ITEMLIST_SCHEMA = {
     {"@type":"ListItem","position":458,"url":"https://gamezipper.com/strimko/","name":"Strimko"},
     {"@type":"ListItem","position":459,"url":"https://gamezipper.com/magnets/","name":"Magnets"},
     {"@type":"ListItem","position":460,"url":"https://gamezipper.com/tatami/","name":"Tatami"},
-    {"@type":"ListItem","position":461,"url":"https://gamezipper.com/sym-a-pix/","name":"Sym-a-Pix"}
+    {"@type":"ListItem","position":461,"url":"https://gamezipper.com/sym-a-pix/","name":"Sym-a-Pix"},
+    {"@type":"ListItem","position":462,"url":"https://gamezipper.com/dot-a-pix/","name":"Dot-a-Pix"}
   ],
-  "numberOfItems":461,
-  "description":"461 free browser games you can play instantly"
+  "numberOfItems":462,
+  "description":"462 free browser games you can play instantly"
 };
